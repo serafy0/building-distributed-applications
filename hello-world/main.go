a68058af6d281da8ca3ff59b082cd87c51ce2359
mohamedelserafy <elserafy02@gmail.com>
2023-06-16 17:07:55 +0300

    custom handler

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,14 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
-
+	router.GET("/:name", IndexHandler)
 	router.Run()
 }
