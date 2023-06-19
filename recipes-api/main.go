43edb65f7a325b795caf190c600edf67eaec1d8c
mohamedelserafy <elserafy02@gmail.com>
2023-06-19 05:34:28 +0300

    configure MongoDB

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -15,19 +15,35 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var recipes []Recipe
+var ctx context.Context
+var err error
+var client *mongo.Client
 
 func init() {
 	recipes = make([]Recipe, 0)
-
+	ctx = context.Background()
+	client, err = mongo.Connect(ctx,
+		options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(),
+		readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
 }
 
 func main() {
