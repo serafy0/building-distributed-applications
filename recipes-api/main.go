bd148f89d5b98ec3c574b21e74531ae3c0623c35
mohamedelserafy <elserafy02@gmail.com>
2023-06-19 23:21:41 +0300

    remaining operations

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -30,13 +30,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var recipes []Recipe
 var ctx context.Context
 var err error
 var client *mongo.Client
 var collection *mongo.Collection
-
-// var collection mongo.Collection
 
 func init() {
 	ctx = context.Background()
@@ -55,8 +52,8 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
-	// router.DELETE("/recipes/:id", DeleteRecipeHandler)
-	// router.GET("/recipes/search", SearchRecipesHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 
 	router.Run()
 }
@@ -175,5 +172,37 @@ func UpdateRecipeHandler(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been updated"})
+
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": objectId})
+	if err != nil {
+		fmt.Print(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	cur, err := collection.Find(ctx, bson.M{"tags": bson.M{"$in": []string{tag}}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer cur.Close(ctx)
+	recipes := make([]Recipe, 0)
+	for cur.Next(ctx) {
+		var recipe Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
+	c.JSON(http.StatusOK, recipes)
 
 }
