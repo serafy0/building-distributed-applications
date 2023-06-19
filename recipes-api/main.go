b2991bb8f63b133bb6b2f93a69da3af02e192a25
mohamedelserafy <elserafy02@gmail.com>
2023-06-19 22:04:09 +0300

    get operation

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -33,9 +34,11 @@ var recipes []Recipe
 var ctx context.Context
 var err error
 var client *mongo.Client
+var collection *mongo.Collection
+
+// var collection mongo.Collection
 
 func init() {
-	recipes = make([]Recipe, 0)
 	ctx = context.Background()
 	client, err = mongo.Connect(ctx,
 		options.Client().ApplyURI(os.Getenv("MONGO_URI")))
@@ -44,6 +47,7 @@ func init() {
 		log.Fatal(err)
 	}
 	log.Println("Connected to MongoDB")
+	collection = client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
 }
 
 func main() {
@@ -76,6 +80,19 @@ type Recipe struct {
 //	'200':
 //		description: Successful operation
 func ListRecipesHandler(c *gin.Context) {
+
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(ctx)
+	recipes := make([]Recipe, 0)
+	for cur.Next(ctx) {
+		var recipe Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
 	c.JSON(http.StatusOK, recipes)
 }
 
