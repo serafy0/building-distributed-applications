ad172f9d328fed3d1f99ed1399b2d09d9bd7a34f
mohamedelserafy <elserafy02@gmail.com>
2023-06-20 01:50:50 +0300

    caching with redis

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -16,11 +16,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"recipes-api/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -29,6 +31,7 @@ import (
 var recipesHandler *handlers.RecipesHandler
 
 func init() {
+
 	ctx := context.Background()
 	client, err := mongo.Connect(ctx,
 		options.Client().ApplyURI(os.Getenv("MONGO_URI")))
@@ -41,7 +44,16 @@ func init() {
 	}
 	log.Println("Connected to MongoDB")
 	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
-	recipesHandler = handlers.NewRecipeHandler(ctx, collection)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	status := redisClient.Ping(ctx)
+	fmt.Println(status)
+
+	recipesHandler = handlers.NewRecipeHandler(ctx, collection, redisClient)
 }
 
 func main() {
