50ce1e944975730355b6b24775396ed8a7d1c407
mohamedelserafy <elserafy02@gmail.com>
2023-06-19 05:09:02 +0300

    setup swagger

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -1,3 +1,17 @@
+// Recipes API
+//
+// This is a sample recipes API.
+//
+//   Schemes: http
+//   Host: localhost:8080
+//   BasePath: /
+//   Version: 1.0.0
+//   Consumes:
+//   - application/json
+//
+//   Produces:
+//   - application/json
+// swagger:meta
 package main
 
 import (
@@ -36,6 +50,15 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+// ---
+// produces:
+// - application/json
+// responses:
+//
+//	'200':
+//		description: Successful operation
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
@@ -56,6 +79,26 @@ func NewRecipeHandler(c *gin.Context) {
 
 }
 
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+// Update an existing recipe
+// ---
+// parameters:
+// - name: id
+//    in: path
+//    description: ID of the recipe
+//    required: true
+//    type: string
+// produces:
+// - application/json
+//
+// responses:
+//
+//	200:
+//		description: Successful operation
+//	400:
+//		description: Invalid input
+//	404:
+//		description: Invalid recipe ID
 func UpdateRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
 	var recipe Recipe
